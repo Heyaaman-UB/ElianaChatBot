ce078731e4b9fe565f59e46bbd0112184ca23a8f
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-04-18 17:30:10 +0530

    Create eliana.py

---
+++ eliana.py
@@ -0,0 +1,54 @@
+import asyncio
+import aiohttp
+import re
+from config import token, owner_id, bot_id
+from pyrogram import Client, filters
+
+eliana = Client(
+    ":memory:",
+    token="token Api here",
+    api_id=2442183,
+    api_hash="66c6799e1b784aff78bad680cd8362db",
+)
+
+
+async def chatbot(text):
+    url = f"https://elianaapi.herokuapp.com/eliana/chatbot?text={text}&name=botname"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as res:
+            res = await res.json()
+            text = res["message"]
+            return text
+
+@eliana.on_message(filters.command("about") & ~filters.edited)
+async def about(_, message):
+    await message.reply_text(
+        "[Eliana Api](https://elianaapi.herokuapp.com/)"
+        + " | [Owner](https://github.com/Red-Aura/Eliana-Api)", disable_web_page_preview=True)
+
+
+@eliana.on_message(filters.command("eliana") & ~filters.edited)
+async def start(_, message):
+    await eliana.send_chat_action(message.chat.id, "typing")
+    await message.reply_text(
+        "**I Am Eliana Chatbot**"
+    )
+
+
+
+@eliana.on_message(
+    ~filters.private 
+    & ~filters.command("help")
+    & ~filters.edited
+)
+async def chat(_, message):
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user.id == bot_id:
+            return
+        await eliana.send_chat_action(message.chat.id, "typing")
+        if not message.text:
+            query = "Hello"
+        else:
+            query = message.text
+        if len(query) > 50:
+    
