10e2515609c52ca11c664e967f1e661f3275cae4
Kai <90447061+Heyaaman-UB@users.noreply.github.com>
2021-11-02 11:31:34 +0530

    Update eliana.py

--- eliana.py
+++ eliana.py
@@ -13,30 +13,30 @@ eliana = Client(
 
 
 async def chatbot(text):
-    url = f"https://elianaapi.herokuapp.com/eliana/chatbot?text={text}&name=eliana"
+    url = f"https://kazukoapi.herokuapp.com/kazuko/chatbot?text={text}&name=eliana"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as res:
             res = await res.json()
             text = res["message"]
             return text
 
-@eliana.on_message(filters.command("about") & ~filters.edited)
+@kazuko.on_message(filters.command("about") & ~filters.edited)
 async def about(_, message):
     await message.reply_text(
-        "[Eliana Api](https://elianaapi.herokuapp.com/)"
-        + " | [Owner](https://github.com/Red-Aura/Eliana-Api)", disable_web_page_preview=True)
+        "[Kazuko Api](https://elianaapi.herokuapp.com/)"
+        + " | [Owner](https://github.com/heyaaman/KazukoBot)", disable_web_page_preview=True)
 
 
-@eliana.on_message(filters.command("eliana") & ~filters.edited)
+@kazuko.on_message(filters.command("kazuko") & ~filters.edited)
 async def start(_, message):
     await eliana.send_chat_action(message.chat.id, "typing")
     await message.reply_text(
-        "**I Am Eliana Chatbot**"
+        "**I Am Kazuko an advanced Group managing bot and a chatbot**"
     )
 
 
 
-@eliana.on_message(
+@kazuko.on_message(
     ~filters.private 
     & ~filters.command("help")
     & ~filters.edited
@@ -45,7 +45,7 @@ async def chat(_, message):
     if message.reply_to_message:
         if not message.reply_to_message.from_user.id == bot_id:
             return
-        await eliana.send_chat_action(message.chat.id, "typing")
+        await kazuko.send_chat_action(message.chat.id, "typing")
         if not message.text:
             query = "Hello"
         else:
@@ -58,24 +58,24 @@ async def chat(_, message):
         except Exception as e:
             res = str(e)
         await message.reply_text(res)
-        await eliana.send_chat_action(message.chat.id, "cancel")
+        await kazuko.send_chat_action(message.chat.id, "cancel")
     else:
         if message.text:
             query = message.text
             if len(query) > 50:
                 return
             if re.search("[.|\n]{0,}[l|L][u|U][n|N][a|A][.|\n]{0,}", query):
-                await eliana.send_chat_action(message.chat.id, "typing")
+                await kazuko.send_chat_action(message.chat.id, "typing")
                 try:
                     res = await chatbot(query)
                     await asyncio.sleep(1)
                 except Exception as e:
                     res = str(e)
                 await message.reply_text(res)
-                await eliana.send_chat_action(message.chat.id, "cancel")
+                await kazuko.send_chat_action(message.chat.id, "cancel")
 
 
-@eliana.on_message(
+@kazuko.on_message(
     filters.private
     & ~filters.command("help")
     & ~filters.edited
@@ -83,7 +83,7 @@ async def chat(_, message):
 async def chatpm(_, message):
     if not message.text:
         return
-    await eliana.send_chat_action(message.chat.id, "typing")
+    await kazuko.send_chat_action(message.chat.id, "typing")
     query = message.text
     if len(query) > 50:
         return
@@ -93,14 +93,14 @@ async def chatpm(_, message):
     except Exception as e:
         res = str(e)
     await message.reply_text(res)
-    await eliana.send_chat_action(message.chat.id, "cancel")
+    await kazuko.send_chat_action(message.chat.id, "cancel")
 
 
 print(
     """
-  Eliana Started
+  Kazuko Started
 """
 )
 
 
-eliana.run()
+kazuko.run()
